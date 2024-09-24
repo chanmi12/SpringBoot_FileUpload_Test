f2bfec509184be0d0a8d2b2762187d73fbc9cbbc
dnldl <dnldlscjf@gmail.com>
2024-09-24 15:12:36 +0900

    WorkRepository.java 중복 삭제

--- src/main/java/com/example/work/WorkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.work;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface WorkRepository extends JpaRepository<Work , Long> {
-    List<Work> findByUserId(Long userId);
-}
