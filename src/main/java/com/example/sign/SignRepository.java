af08c45408d3dfa8d8a97d63469a6cd6fb02480b
dnldl <dnldlscjf@gmail.com>
2024-10-09 17:20:24 +0900

    중복 오류 삭제

--- src/main/java/com/example/sign/SignRepository.java
+++ src/main/java/com/example/sign/SignRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface SignRepository extends JpaRepository<Sign, Long> {
-    List<Sign> finByUserIdAndDeletedFalse(Long userId); //유저의 Sign 목록 조회
-    Optional<Sign> findByIdAndUserIdAndDeletedFalse(Long id , Long userId); //유저의 Sign 조회
+    List<Sign> findByUserIdAndDeletedFalse(Long userId);
 }
