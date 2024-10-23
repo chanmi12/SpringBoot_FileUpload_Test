9a985f616877a6afe5dd295cd8a5bf732dc21eed
dnldl <dnldlscjf@gmail.com>
2024-10-23 19:17:10 +0900

    Sign 휴지통 기능 api 재설정

--- src/main/java/com/example/work/service/WorkService.java
+++ src/main/java/com/example/work/service/WorkService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -163,6 +164,7 @@ public class WorkService {
         }
         Work work = workOpt.get();
         work.setTrashed(true);
+        work.setDeleteDate(LocalDateTime.now());
         workRepository.save(work);
     }
 
