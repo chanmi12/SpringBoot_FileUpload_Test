5d35adf35a450b3151cda3d33cc2d277812627ee
dnldl <dnldlscjf@gmail.com>
2024-11-04 17:13:45 +0900

    feat: 히스넷 로그인 되는 부분만 구현

--- src/main/java/com/example/auth/controller/AuthController.java
+++ src/main/java/com/example/auth/controller/AuthController.java
@@ -1,26 +1,26 @@
-//package com.example.auth.controller;
-//
-//import com.example.auth.controller.request.LoginRequest;
-//import com.example.auth.controller.response.LoginResponse;
-//import com.example.auth.dto.AuthDto;
-//import com.example.auth.service.AuthService;
-//import com.example.auth.service.HisnetLoginService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//@RequiredArgsConstructor
-//public class AuthController {
-//
-//    private final AuthService authService;
-//    private final HisnetLoginService hisnetLoginService;
-//    @PostMapping("/login")
-//    public ResponseEntity<LoginResponse> Login(@RequestBody LoginRequest request) {
-//        return ResponseEntity.ok(LoginResponse.from(authService.login(hisnetLoginService.callHisnetLoginApi(AuthDto.from(request)))));
-//    }
-//}
+package com.example.auth.controller;
+
+import com.example.auth.controller.request.LoginRequest;
+import com.example.auth.controller.response.LoginResponse;
+import com.example.auth.dto.AuthDto;
+import com.example.auth.service.AuthService;
+import com.example.auth.service.HisnetLoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final HisnetLoginService hisnetLoginService;
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> Login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(LoginResponse.from(authService.login(hisnetLoginService.callHisnetLoginApi(AuthDto.from(request)))));
+    }
+}
--- src/main/java/com/example/auth/service/AuthService.java
+++ src/main/java/com/example/auth/service/AuthService.java
@@ -1,52 +1,52 @@
-//package com.example.auth.service;
-//
-//import com.example.auth.dto.AuthDto;
-//import com.example.auth.exception.DoNotExistException;
-//import com.example.auth.util.JwtUtil;
-//import com.example.user.User;
-//import com.example.user.UserRepository;
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.beans.factory.annotation.Value;
-//
-//import java.util.Optional;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class AuthService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Value("${custom.jwt.secret}")
-//    private String SECRET_KEY;
-//
-//    public User getLoginUser(String uniqueId) {
-//        return userRepository
-//                .findByUniqueId(uniqueId)
-//                .orElseThrow(() -> new DoNotExistException("해당 유저가 없습니다."));
-//    }
-//
-//    public AuthDto login(AuthDto dto) {
-//        Optional<User> user = userRepository.findByUniqueId(dto.getUniqueId());
-//
-//        if (user.isEmpty()) {
-//            // Register a new user if they don't exist
-//            User newUser = User.from(dto);
-//            userRepository.save(newUser);
-//
-//            return AuthDto.builder()
-//                    .token(JwtUtil.createToken(newUser.getUniqueId(), newUser.getName(), SECRET_KEY))
-//                    .build();
-//
-//        } else {
-//            // Update the existing user if they are found
-//            user.get().update(dto);
-//
-//            return AuthDto.builder()
-//                    .token(JwtUtil.createToken(user.get().getUniqueId(), user.get().getName(), SECRET_KEY))
-//                    .build();
-//        }
-//    }
-//}
+package com.example.auth.service;
+
+import com.example.auth.dto.AuthDto;
+import com.example.auth.exception.DoNotExistException;
+import com.example.auth.util.JwtUtil;
+import com.example.user.User;
+import com.example.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    @Value("${custom.jwt.secret}")
+    private String SECRET_KEY;
+
+    public User getLoginUser(String uniqueId) {
+        return userRepository
+                .findByUniqueId(uniqueId)
+                .orElseThrow(() -> new DoNotExistException("해당 유저가 없습니다."));
+    }
+
+    public AuthDto login(AuthDto dto) {
+        Optional<User> user = userRepository.findByUniqueId(dto.getUniqueId());
+
+        if (user.isEmpty()) {
+            // Register a new user if they don't exist
+            User newUser = User.from(dto);
+            userRepository.save(newUser);
+
+            return AuthDto.builder()
+                    .token(JwtUtil.createToken(newUser.getUniqueId(), newUser.getName(), SECRET_KEY))
+                    .build();
+
+        } else {
+            // Update the existing user if they are found
+            user.get().update(dto);
+
+            return AuthDto.builder()
+                    .token(JwtUtil.createToken(user.get().getUniqueId(), user.get().getName(), SECRET_KEY))
+                    .build();
+        }
+    }
+}
