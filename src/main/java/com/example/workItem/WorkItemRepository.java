250c20ba2f097eee0c2140c8814b5fdcaf3c63b6
dnldl <dnldlscjf@gmail.com>
2024-11-12 19:14:14 +0900

    feat: 코드에 대한 주석 추가

--- src/main/java/com/example/workItem/WorkItemRepository.java
+++ src/main/java/com/example/workItem/WorkItemRepository.java
@@ -12,15 +12,17 @@ import java.util.Optional;
 
 @Repository
 public interface WorkItemRepository extends JpaRepository<WorkItem,Long> {
+    // 특정 작업 ID에 대한 모든 작업 항목을 반환하는 쿼리
     @EntityGraph(attributePaths = {"sign"})
     List<WorkItem> findByWorkId(Long workId);
+    // 특정 작업 ID 및 사용자 ID로 모든 작업 항목을 반환하는 쿼리
     @Query("SELECT wi FROM WorkItem wi WHERE wi.work.id = :workId AND wi.user.id = :otherId")
     List<WorkItem> findByWorkIdAndOtherUserId(@Param("workId") Long workId, @Param("otherId") Long otherId);
-
-    List<WorkItem> findByWorkIdAndUserId(Long workId, Long userId);
+    // 특정 사용자 ID 및 작업 ID로 모든 작업 항목을 반환하는 쿼리
     List<WorkItem> findByUserIdAndWorkId(Long userId, Long workId);
 
     @EntityGraph(attributePaths = {"sign"})
+    // 특정 작업 항목 ID로 작업 항목을 반환하는 쿼리
     Optional<WorkItem> findById(Long workItemId);
     @Query("SELECT DISTINCT wi.user.id FROM WorkItem wi WHERE wi.work.id = :workId")
     List<Long> findDistinctUserIdsByWorkId(@Param("workId") Long workId); // 특정 Work에 대한 모든 User ID를 반환하는 쿼리
@@ -33,4 +35,7 @@ public interface WorkItemRepository extends JpaRepository<WorkItem,Long> {
     int countDistinctUsersByWorkId(@Param("workId") Long workId);
 
     List<WorkItem> findByWorkIdAndAutoCreatedFalse(Long workId);
+
+    @Query("SELECT wi FROM WorkItem wi WHERE wi.work.id = :workId AND wi.autoCreated = false")
+    List<WorkItem> findNonAutoCreatedWorkItemsByWorkId(@Param("workId") Long workId);
 }
