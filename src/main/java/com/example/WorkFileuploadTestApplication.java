533b8ae7e9651b4ca1c54c3250c85092478bd5aa
dnldl <dnldlscjf@gmail.com>
2024-11-12 19:12:08 +0900

    feat: jar 에서 war로 변경

--- src/main/java/com/example/WorkFileuploadTestApplication.java
+++ src/main/java/com/example/WorkFileuploadTestApplication.java
@@ -2,10 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.swing.*;
 
 @SpringBootApplication
-public class WorkFileuploadTestApplication {
-
+public class WorkFileuploadTestApplication  extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WorkFileuploadTestApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WorkFileuploadTestApplication.class, args);
     }
